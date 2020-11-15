81b54dfba121fecdc4fa05164bfc00b8627ad05a
Simon Shine <shreddedglory@gmail.com>
2020-11-15 10:19:45 +0100

    Make fixpoint function use Data.Function.fix
    Rephrase some of the comments.
    
    Rephrase `translate` and `labelPositions` so they read clearer.

--- src/EVM/Opcode/Labelled.hs
+++ src/EVM/Opcode/Labelled.hs
@@ -19,8 +19,10 @@ module EVM.Opcode.Labelled
   , LabelledOpcode
   , TranslateError(..)
   , translate
+  , labelPositions
   ) where
 
+import           Data.Function (fix)
 import           Data.List (group, sort, foldl')
 import qualified Data.Map as Map
 import           Data.Map (Map)
@@ -42,76 +44,99 @@ type Label = Text
 -- All other opcodes remain the same.
 type LabelledOpcode = Opcode' Label
 
--- | Translation of a 'LabelledOpcode' into 'PositionalOpcode' may fail if
--- either a jump is made to a labelled 'JUMPDEST' that does not occur (is
--- missing), or a labelled 'JUMPDEST' occurs twice in different positions (is
--- a duplicate).
+-- | Translation of 'LabelledOpcode's into 'PositionalOpcode's may fail if
+-- a jump is made to a non-occurring 'JUMPDEST' or a 'JUMPDEST' occurs twice.
+
 data TranslateError = TranslateError
   { translateErrorMissingJumpdests   :: [Label]
   , translateErrorDuplicateJumpdests :: [Label]
   } deriving (Eq, Show)
 
--- | Translate a 'LabelledOpcode' into a list of 'PositionalOpcode' by
--- replacing the labels with absolute positions. The positions are calculated
--- using a fixed-point algorithm so that the (variable) size of a jump itself
--- is accounted for.
+-- | Replace all labels with absolute positions.
 --
--- Labelled jumps don't have a size defined, but the size of a positional jump
--- depends on the address being jumped to. So for example, if jumping to the
--- 'JUMPDEST' on the 256th position in a @['LabelledOpcode']@, this requires
--- a 'PUSH2' instruction which uses an additional byte, which pushes the
--- 'JUMPDEST' one byte ahead.
+-- Positions are calculated by fixed-point iteration to account for variable
+-- sizes of jumps. Labelled jumps don't have a size defined, the size of a
+-- positional jump depends on the address being jumped to.
+--
+-- For example, if jumping to the 'JUMPDEST' on the 256th position in a
+-- @['LabelledOpcode']@, this requires a 'PUSH2' instruction which uses an
+-- additional byte, which pushes the 'JUMPDEST' one byte ahead.
 
 translate :: [LabelledOpcode] -> Either TranslateError [PositionalOpcode]
 translate opcodes = do
   labelMap <- labelPositions opcodes
-  traverse (mapOpcodeM (myMapper (lookup' labelMap))) opcodes
+  traverse (replaceLabel labelMap) opcodes
   where
-    myMapper :: (Label -> Either TranslateError Position)
-             -> OpcodeMapper (Either TranslateError) Label Position
-    myMapper f = OpcodeMapper
+    replaceLabel = mapOpcodeM . jumpMapper . lookupLabel
+
+    -- Apply @f@ to the parameter of 'JUMP's, 'JUMPI's and 'JUMPDEST's
+    jumpMapper f = OpcodeMapper
       { mapOnJump = fmap JUMP . f
       , mapOnJumpi = fmap JUMPI . f
-      , mapOnJumpdest = fmap JUMPDEST .f
+      , mapOnJumpdest = fmap JUMPDEST . f
       , mapOnOther = const (pure Nothing)
       }
 
-    lookup' labelMap label = case Map.lookup label labelMap of
-      Just pos -> Right pos
-      Nothing -> Left (TranslateError [label] [])
+    -- Let @f@ be @'lookupLabel' labelMap@.
+    lookupLabel labelMap label =
+      case Map.lookup label labelMap of
+        Just pos -> Right pos
+        Nothing -> Left (TranslateError [label] [])
+
 
 -- | Extract a @'Map' 'Label' 'Position'@ that describes where each 'JUMPDEST'
 -- is located, taking into account the sizes of all prior opcodes.
+
 labelPositions :: [LabelledOpcode] -> Either TranslateError (Map Label Position)
-labelPositions opcodes =
-  case (jumps `missing` dests, duplicate dests) of
-    ([], []) -> Right (fixpoint opcodes Map.empty)
-    (missing', duplicate') -> Left (TranslateError missing' duplicate')
+labelPositions opcodes
+  | null wildJumps && null duplicateDests = Right (fixpoint opcodes)
+  | otherwise = Left (TranslateError wildJumps duplicateDests)
   where
+    wildJumps :: [Label]
+    wildJumps = jumps `missing` dests
+
+    duplicateDests :: [Label]
+    duplicateDests = duplicate dests
+
     jumps :: [Label]
     jumps = mapMaybe jumpAnnot opcodes
 
     dests :: [Label]
     dests = mapMaybe jumpdestAnnot opcodes
 
--- | Update the position of every 'JUMPDEST'.
+    missing :: Ord a => [a] -> [a] -> [a]
+    missing xs ys = Set.toList (Set.difference (Set.fromList xs) (Set.fromList ys))
+
+    duplicate :: Ord a => [a] -> [a]
+    duplicate = concatMap (take 1) . filter ((> 1) . length) . group . sort
+
+-- | Extract a 'Map' the position of every 'JUMPDEST'.
 --
 -- Do this by keeping track of the current position.
 --
--- For jumps, increase the current position by however much
---
--- FIXME: Use 'Data.Function.fix'.
-fixpoint :: [LabelledOpcode] -> Map Label Position -> Map Label Position
-fixpoint opcodes labelMap =
-  case step opcodes labelMap of
+-- This function may not terminate for all inputs!
+
+fixpoint :: [LabelledOpcode] -> Map Label Position
+fixpoint opcodes = flip fix Map.empty $ \go labelMap ->
+  case step labelMap opcodes of
     (True, _, labelMap') -> labelMap'
-    (False, _, labelMap') -> fixpoint opcodes labelMap'
+    (False, _, labelMap') -> go labelMap'
 
-step :: [LabelledOpcode] -> Map Label Position -> (Bool, Position, Map Label Position)
-step opcodes labelMap = foldl' align (True, 0, labelMap) opcodes
+-- | A single step in the fixpoint function is going over every opcode and
+-- checking if its position is already aligned and updating the map of
+-- positions otherwise.
+step :: Map Label Position
+     -> [LabelledOpcode]
+     -> (Bool, Position, Map Label Position)
+step labelMap = foldl' align (True, 0, labelMap)
 
-align :: (Bool, Position, Map Label Position) -> LabelledOpcode
+align :: (Bool, Position, Map Label Position)
+      -> LabelledOpcode
       -> (Bool, Position, Map Label Position)
+
+-- When encountering a 'JUMPDEST' that was either not seen before, or was seen
+-- at another offset, it hasn't been aligned. In that case, update the 'Map'
+-- and signal that another iteration of 'fixpoint' is necessary.
 align (done, currentBytePos, labelMap) (JUMPDEST label) =
   let aligned = Map.lookup label labelMap == Just currentBytePos
   in ( done && aligned
@@ -119,18 +144,14 @@ align (done, currentBytePos, labelMap) (JUMPDEST label) =
      , Map.insert label currentBytePos labelMap
      )
 
+-- When encountering a 'JUMP' or a 'JUMPI', check if the destination 'JUMPDEST'
+-- was seen before. If so, increment the running offset with the size of a jump
+-- to that 'JUMPDEST'. If not, the offset is still approximate.
 align (done, currentBytePos, labelMap) (jumpAnnot -> Just label) =
   case Map.lookup label labelMap of
     Just bytePos -> ( done, currentBytePos + jumpSize bytePos, labelMap )
     Nothing      -> ( False, currentBytePos + jumpSize 0,      labelMap )
 
+-- For any straight-line opcode, just increment the offset with its size.
 align (done, currentBytePos, labelMap) opcode =
   ( done, currentBytePos + opcodeSize (concrete opcode), labelMap )
-
--- | Complete difference: Get the elements of 'xs' that do not occur in 'ys'.
-missing :: Ord a => [a] -> [a] -> [a]
-missing xs ys = Set.toList (Set.difference (Set.fromList xs) (Set.fromList ys))
-
--- | Get a sublist of elements that occur more than once.
-duplicate :: Ord a => [a] -> [a]
-duplicate = concatMap (take 1) . filter ((> 1) . length) . group . sort

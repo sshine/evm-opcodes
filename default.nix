1138b743d279ba47cde5e5cdfb6d687ce843a67f
sshine <simon@simonshine.dk>
2026-01-25 12:24:22 +0100

    docs: Explain default.nix better

--- default.nix
+++ default.nix
@@ -1,23 +1,30 @@
 let
+  # Use nixpkgs available on the system
   pkgs = import <nixpkgs> {};
+  haskellDependencies = with pkgs.haskellPackages; [
+    stack
+    cabal-install
+    haskell-language-server
+    hlint
+    ghcid
+    hpack
+  ];
+
+  # Build a Haskell package with those dependencies
+  addBuildTools = pkgs.haskell.lib.addBuildTools;
   package = pkgs.haskellPackages.developPackage {
     root = ./.;
-    modifier = drv:
-      pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages; [
-        stack
-        cabal-install
-        haskell-language-server
-        hlint
-        ghcid
-        hpack
-      ]);
+    modifier = drv: addBuildTools drv haskellDependencies;
   };
-in
-  package.overrideAttrs (old: {
+
+  # Extend the build environment with pre-commit installed and activated
+  withPreCommitHook = old: {
     nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.pre-commit ];
     shellHook = (old.shellHook or "") + ''
       if [ -d .git ]; then
         pre-commit install --hook-type pre-push
       fi
     '';
-  })
+  };
+in
+  package.overrideAttrs withPreCommitHook
